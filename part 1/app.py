ba92472690c6e42c7e97034121a058e6ac57b126
ItProjectsSchool <65037900+ItProjectsSchool@users.noreply.github.com>
2020-09-14 13:14:23 +0300

    Update app.py

--- part 1/app.py
+++ part 1/app.py
@@ -1,1 +1,12 @@
+# in this part is created Flask object for future work
+from flask import Flask
+app = Flask(__name__)
 
+# In this part URLs are specified
+@app.route('/')
+def start():
+    return "Hello World!"
+
+# This part is used to launch out app
+if __name__ == '__main__':
+    app.run()
